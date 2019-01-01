60a54c7ceaff0a1750a4c9116dd34668ec6d6ace
ricardohoil <46288056+ricardohoil@users.noreply.github.com>
2019-01-01 09:10:07 -0600

    Add files via upload

--- js/plantilla.js
+++ js/plantilla.js
@@ -1,263 +1,70 @@
-/*=============================================
-PLANTILLA
-=============================================*/
+/* iCheck */
+$('input').iCheck({
+	checkboxClass: 'icheckbox_square-blue',
+	radioClass: 'iradio_square-blue',
+	increaseArea: '20%' // optional
+});
 
-var rutaOculta = $("#rutaOculta").val();
+/* jQueryKnob */
+$('.knob').knob();
 
-// Herramienta TOOLTIP
-$('[data-toggle="tooltip"]').tooltip(); 
+/* SideBar Menu */
+$('.sidebar-menu').tree();
 
-$.ajax({
+//Colorpicker
+$('.my-colorpicker').colorpicker();
 
-	url:rutaOculta+"ajax/plantilla.ajax.php",
-	success:function(respuesta){
-
-		var colorFondo = JSON.parse(respuesta).colorFondo;
-		var colorTexto = JSON.parse(respuesta).colorTexto;
-		var barraSuperior = JSON.parse(respuesta).barraSuperior;
-		var textoSuperior = JSON.parse(respuesta).textoSuperior;
-		
-		$(".backColor, .backColor a").css({"background": colorFondo,
-											"color": colorTexto})
-
-		$(".barraSuperior, .barraSuperior a").css({"background": barraSuperior, 
-			                                       "color": textoSuperior})
-
-	}
-
+//Tags Input
+$(".tagsInput").tagsinput({
+	maxTags: 10,
+	confirmKeys: [44],
+	cancelConfirmKeysOnEmpty: false,
+ 	trimValue: false
 })
 
-/*=============================================
-CUADRÍCULA O LISTA
-=============================================*/
+$(".bootstrap-tagsinput").css({"padding":"11px",
+							   "width":"100%",
+ 							   "border-radius":"1px"})
 
-var btnList = $(".btnList");
-var btnGrid = $(".btnGrid");
-
-for(var i = 0; i < btnList.length; i++){
-
-	$(btnGrid[i]).addClass("backColor");
-
-	$("#btnGrid"+i).click(function(){
-
-		var numero = $(this).attr("id").substr(-1);
-
-		$(".list"+numero).hide();
-		$(".grid"+numero).show();
-
-		$("#btnGrid"+numero).addClass("backColor");
-		$("#btnList"+numero).attr("style","");
-		$("#btnList"+numero).removeClass("backColor");
-	})
-
-	$("#btnList"+i).click(function(){
-
-		var numero = $(this).attr("id").substr(-1);
-
-		$(".list"+numero).show();
-		$(".grid"+numero).hide();
-		
-		$("#btnList"+numero).addClass("backColor");
-		$("#btnGrid"+numero).attr("style","");
-		$("#btnGrid"+numero).removeClass("backColor");
-
-	})
-
-}
-
-/*=============================================
-EFECTOS CON EL SCROLL
-=============================================*/
-
-$(window).scroll(function(){
-
-	var scrollY = window.pageYOffset;
-
-	if(window.matchMedia("(min-width:768px)").matches){
-
-		if($(".banner").html() != null){
-
-			if(scrollY < ($(".banner").offset().top)-150){
-
-				$(".banner img").css({"margin-top":-scrollY/3+"px"})
-
-			}else{
-
-				scrollY = 0;
-			}
-
-		}
-
-	}	
-	
-})
-
-$.scrollUp({
-
-	scrollText:"",
-	scrollSpeed: 2000,
-	easingType: "easeOutQuint"
-
+//Datepicker	
+$('.datepicker').datepicker({
+	format: 'yyyy-mm-dd 23:59:59',
+	startDate: '0d'
 });
 
 /*=============================================
-MIGAS DE PAN
+CORRECCIÓN BOTONERAS OCULTAS BACKEND	
 =============================================*/
 
-var pagActiva = $(".pagActiva").html();
+if(window.matchMedia("(max-width:767px)").matches){
+	
+	$("body").removeClass('sidebar-collapse');
 
-if(pagActiva != null){
-
-	var regPagActiva = pagActiva.replace(/-/g, " ");
-
-	$(".pagActiva").html(regPagActiva);
-
-}
-
-/*=============================================
-ENLACES PAGINACIÓN
-=============================================*/
-
-var url = window.location.href;
-
-var indice = url.split("/");
-
-var pagActual =indice[5];
-
-if(isNaN(pagActual)){
-
-   $("#item1").addClass("active");
-   
 }else{
 
-   $("#item"+pagActual).addClass("active");
- 
+	$("body").addClass('sidebar-collapse');
 }
 
 /*=============================================
-OFERTAS
+ACTIVAR SIDEBAR
 =============================================*/
-$(".cerrarOfertas").click(function(){
 
-	$(this).parent().remove();
+$(document).on("click", ".sidebar-menu li", function(){
+
+	localStorage.setItem("botonera", $(this).children().attr("href"));
 
 })
 
-if($("#moduloOfertas").children().length == 0){
+if(localStorage.getItem("botonera") == null){
 
-	$("#moduloOfertas").html(
-		'<div class="jumbotron">'+
+	$(".sidebar-menu li").removeClass("active");
 
-		'<h1 class="text-center">Lo sentimos</h1>'+
+	$(".sidebar-menu li a[href='inicio']").parent().addClass("active")
+	
+}else{
 
-			'<h3 class="text-center">¡En estos momentos no hay ofertas disponibles!</h3>'+ 
-				 
-		'</div>'
+	$(".sidebar-menu li").removeClass("active");
 
-	);
+	$("a[href='"+localStorage.getItem("botonera")+"']").parent().addClass("active")
+	
 }
-
-/*=============================================
-CONTADOR DE TIEMPO
-=============================================*/
-
-var finOferta = $(".countdown");
-var fechaFinOferta = [];
-
-for(var i = 0; i < finOferta.length; i++){
-
-	fechaFinOferta[i] = $(finOferta[i]).attr("finOferta");
-
-	$(finOferta[i]).dsCountDown({
-
-	endDate: new Date(fechaFinOferta[i]),
-
-	theme: 'flat', 
-
-	titleDays: 'Días', 
-
-	titleHours: 'Horas', 
-
-	titleMinutes: 'Minutos', 
-
-	titleSeconds: 'Segundos' 
-
-
-	});
-
-
-}
-
-/*=============================================
-CONTADOR DE TIEMPO OFERTAS PRODUCTOS
-=============================================*/
-
-var finOferta2 = $('.countdown2').attr("finOferta");
-
-$('.countdown2').dsCountDown({
-	endDate: new Date(finOferta2),
-	theme: 'black',
-	titleDays: 'Dias', 
-	titleHours: 'Horas', 
-	titleMinutes: 'Min', 
-	titleSeconds: 'Seg'
-
-});
-
-
-/*=============================================
-EVENTOS PIXEL DE FACEBOOK
-=============================================*/
-
-$(".pixelCategorias").click(function(){
-
-	var titulo = $(this).attr("titulo");
-
-	fbq('track', 'Categoria '+titulo, {
-
-		title: titulo
-
-	})
-
-})
-
-$(".pixelSubCategorias").click(function(){
-
-	var titulo = $(this).attr("titulo");
-
-	fbq('track', 'Subcategoria '+titulo, {
-
-		title: titulo
-
-	})
-
-})
-
-
-$(".pixelOferta").click(function(){
-
-	var titulo = $(this).attr("titulo");
-
-	fbq('track', 'Oferta '+titulo, {
-
-		title: titulo
-
-	})
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
